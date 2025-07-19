ab85e425fc2183c0eb3e0dc33fccdb820083a785
Banaan Kiamanesh <kiamaneshbanaan@gmail.com>
2025-07-19 08:32:58 +0200

    Refactor UR10 example and remove symbolic simplification
    Moved UR10Dynamics.m to examples/UR10/ and updated the script to generate and test symbolic kinematics and dynamics code for the UR10 robot. Removed unnecessary 'simplify' calls from ManipulatorDynamics and ManipulatorKinematics to improve performance and code generation speed.

---
+++ examples/UR10/UR10Dynamics.m
@@ -0,0 +1,62 @@
+clear
+close all
+clc
+
+%% UR10 Dynamical Properties
+M = [   7.1,  12.7,   4.27,        2,      2,  0.365];
+L = [0.1273, 0.612, 0.5723, 0.163941, 0.1157, 0.0922];
+R = [   0.2,  0.15,   0.15,      0.1,    0.1,    0.1];
+
+I = cell(6, 1);
+I{1} = diag([M(1)/12*(L(1)^2+3*R(1)^2),           0.5*M(1)*R(1)^2, M(1)/12*(L(1)^2+3*R(1)^2) ]);
+I{2} = diag([          0.5*M(2)*R(2)^2, M(2)/12*(L(2)^2+3*R(2)^2), M(2)/12*(L(2)^2+3*R(2)^2) ]);
+I{3} = diag([          0.5*M(3)*R(3)^2, M(3)/12*(L(3)^2+3*R(3)^2), M(3)/12*(L(3)^2+3*R(3)^2) ]);
+I{4} = diag([M(4)/12*(L(4)^2+3*R(4)^2),           0.5*M(4)*R(4)^2, M(4)/12*(L(4)^2+3*R(4)^2) ]);
+I{5} = diag([M(5)/12*(L(5)^2+3*R(5)^2),           0.5*M(5)*R(5)^2, M(5)/12*(L(5)^2+3*R(5)^2) ]);
+I{6} = diag([M(6)/12*(L(6)^2+3*R(6)^2), M(6)/12*(L(6)^2+3*R(6)^2),           0.5*M(6)*R(6)^2 ]);
+
+% DH Table
+alpha = [   pi/2,       0,        0,     pi/2,  -pi/2,      0];
+a     = [      0,  -0.612,  -0.5723,        0,      0,      0];
+d     = [ 0.1273,       0,        0, 0.163941, 0.1157, 0.0922];
+theta = zeros(1, 6);
+type = ["r", "r", "r", "r", "r", "r"];
+
+% Data Structure Creation
+DH     = DHStruct('theta', theta, 'a', a, 'd', d, 'alpha', alpha, 'type', type);
+DynPar = DynStruct('DH', DH, 'Radius', R, 'Inertia', I, 'Mass', M, 'Length', L);
+
+
+%% Symbolic Model & Code Generation
+kin = ManipulatorKinematics(DH);
+dyn = ManipulatorDynamics(DynPar);
+
+% forward kinematics
+kin.CalculateFK('Rows', 1:6, 'Generate', 'mfile', 'File', 'ur10_fk');
+
+% geometric & analytical Jacobians
+kin.Jacobian('Type', 'geometric' , 'Generate', 'mfile', 'File', 'ur10_jac_geo');
+kin.Jacobian('Type', 'analytical', 'Generate', 'mfile', 'File', 'ur10_jac_ana');
+
+% dynamics: B, C, g 
+dyn.MassMatrix('Generate', 'mfile', 'File', 'ur10_dyn');
+dyn.Coriolis  ('Generate', 'mfile', 'File', 'ur10_dyn');
+dyn.Gravity   ('Generate', 'mfile', 'File', 'ur10_dyn');
+
+fprintf('\nCode Generation Complete!\n');
+
+%% Quick Numeric Smoke-Test
+q    = zeros(6, 1);
+qdot = randn(6, 1);
+
+Pose = ur10_fk(q);
+Jg   = ur10_jac_geo(q);
+B    = ur10_dyn_B(q);
+C    = ur10_dyn_C(q, qdot);
+g    = ur10_dyn_g(q);
+
+disp('FK at Zero Config:');   disp(Pose)
+disp('Jacobian:');            disp(Jg)
+disp('Mass-Matrix:');         disp(B)
+disp('Coriolis:');            disp(C)
+disp('Gravity Vector:');      disp(g)
